f7e2d2dc019d86c48e5cfe495396642c78df92c7
Erik Rundberg <erik@rundberg.ax>
2022-09-10 01:48:33 +0200

    Added baseUrl.

--- src/models/docs.js
+++ src/models/docs.js
@@ -1,18 +1,22 @@
 const docs = {
+    baseUrl: window.location.href.includes("localhost") ?
+        "http://localhost:1338" :
+        "https://jsramverk-editor-erru17.azurewebsites.net",
+
     getAllDocs: async function getAllDocs() {
-        const response = await fetch(`http://localhost:1338/docs`);
+        const response = await fetch(`${this.baseUrl}/docs`);
         const result = await response.json();
 
         return result.data;
     },
     getDoc: async function getDoc(id) {
-        const response = await fetch(`http://localhost:1338/docs/${id}`);
+        const response = await fetch(`${this.baseUrl}/docs/${id}`);
         const result = await response.json();
 
         return result.data;
     },
     createDoc: async function createDoc(doc) {
-        const response = await fetch(`http://localhost:1338/docs`, {
+        const response = await fetch(`${this.baseUrl}/docs`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
